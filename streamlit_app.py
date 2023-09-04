61f5dc8b79cc0a070c1d906903ab20e3b425349e
Vikash Kumar <sunnygupta25@gmail.com>
2023-09-04 16:47:48 -0600

    Update streamlit_app.py
    replace watermelon with kiwi

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+# streamlit.text(fruityvice_response.json())
 
 # take the json version of the response and normalize it
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
